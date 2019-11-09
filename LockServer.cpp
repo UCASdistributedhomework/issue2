5f5dfd43f98d84ed32442d211e2d1051a93500d6
Im-fengyin <37497926+Im-fengyin@users.noreply.github.com>
2019-11-09 22:15:59 +0800

    Add files via upload

--- LockServer.cpp
+++ LockServer.cpp
@@ -12,6 +12,8 @@
 #include <iostream>
 #include <stdexcept>
 #include <sstream>
+#include <string.h>
+#include <queue>
 
 #include "gen-cpp/Locker.h"
 
@@ -24,34 +26,88 @@ using namespace apache::thrift::server;
 
 
 class LockerHandler : virtual public LockerIf {
+ private:
+  const int  MAXNCLIENT = 100000;
+  queue<int> client_request_queue;
+  bool client_request_flag[MAXNCLIENT];
+  bool client_alive_flag[MAXNCLIENT];
+  int lock_client;//-1 is free
  public:
   LockerHandler() {
     // Your initialization goes here
+    memset(client_request_flag,0,sizeof(client_request_flag));
+    memset(client_alive_flag,0,sizeof(client_alive_flag));
+    request_client_queue.clear();
+    lock_client = -1;
   }
 
   bool client_register(const int32_t client_id, const bool is_retry) {
     // Your implementation goes here
+    if(client_id>=MAXNCLIENT){
+      printf("warning : client_id is outrange!\n");
+      return false;
+    }
+    client_alive_flag[client_id] = true;
+    client_request_flag[client_id] = false;
     printf("client_register\n");
+    return true;
   }
 
   bool client_exit(const int32_t client_id, const bool is_retry) {
     // Your implementation goes here
+    client_request_flag[client_id] = false;
+    client_alive_flag[client_id] = false;
+    if(lock_client==client_id){
+      lock_client = -1;
+    }
     printf("client_exit\n");
+    return true;
   }
 
   bool lock_request_register(const int32_t client_id, const bool is_retry) {
     // Your implementation goes here
-    printf("lock_request_register\n");
+    if(client_alive_flag[client_id]){
+      if(!client_request_flag[client_id]){
+        if(lock_client == -1){
+          // no client before ,get the key
+          lock_client = client_id;
+        }else{
+          // wait the key
+          client_request_flag[client_id] = true;
+          client_request_queue.push(client_id);
+        }
+        printf("lock_request_register\n");
+        return true;
+      }
+    }
+    printf("warning : bad request\n");
+    return false;
   }
 
   bool lock_request_check(const int32_t client_id, const bool is_retry) {
     // Your implementation goes here
+    return lock_client == client_id;
     printf("lock_request_check\n");
   }
 
   bool lock_request_release(const int32_t client_id, const bool is_retry) {
     // Your implementation goes here
-    printf("lock_request_release\n");
+    if(lock_client==client_id){
+      lock_client = -1;
+      while(!client_request_queue.isempty()){
+        int client = client_request_queue.front();
+        client_request_queue.pop();
+        if(client_request_queue[client]){
+          lock_client = client;
+          client_request_queue[lock_client] = false;
+          break;
+        }
+      }
+      printf("lock_request_release\n");
+      return true;
+    }
+    printf("warning : invalid release\n");
+    return false;
   }
 
 };
